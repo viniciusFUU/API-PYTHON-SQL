8eeefad3de90f7b93b404a1f3551063e741876c3
Vinícius Monteiro <107506027+viniciusFUU@users.noreply.github.com>
2024-09-09 13:54:45 -0300

    Update crud_python.py

--- crud_python.py
+++ crud_python.py
@@ -10,27 +10,26 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 #create
-# nome_produto = input("DIgite o nome do produto: ")
-# valor = int(input("Valor do produto: "))
-
-# comando_creat = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
-# cursor.execute(comando_creat)
-# conexao.commit() #caso mude algo no banco de dados
+nome_produto = input("DIgite o nome do produto: ")
+valor = int(input("Valor do produto: "))
+comando_creat = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+cursor.execute(comando_creat)
+conexao.commit()
 
 
 # read
-# comando_read = 'SELECT * FROM vendas'
-# cursor.execute(comando_read)
-# resultado = cursor.fetchall()
-# print(resultado)
+comando_read = 'SELECT * FROM vendas'
+cursor.execute(comando_read)
+resultado = cursor.fetchall()
+print(resultado)
 
 
 #update
-# novo_nome_produto = input("Digite o nome do produto que irá alterar o valor: ")
-# novo_valor = int(input("Digite o novo valor do produto: "))
-# comando_update = f'UPDATE vendas SET valor = {novo_valor} WHERE nome_produto = "{novo_nome_produto}"'
-# cursor.execute(comando_update)
-# conexao.commit()
+novo_nome_produto = input("Digite o nome do produto que irá alterar o valor: ")
+novo_valor = int(input("Digite o novo valor do produto: "))
+comando_update = f'UPDATE vendas SET valor = {novo_valor} WHERE nome_produto = "{novo_nome_produto}"'
+cursor.execute(comando_update)
+conexao.commit()
 
 
 #delete
